d0a02f95250819b95ca1ad60e7b4c238c7067942
katrinavalencia12 <kkvalencia3@icloud.com>
2025-04-30 10:31:50 -0400

    all project files

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput, verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            //move the vehicle forward
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+            Debug.Log(Time.deltaTime * turnSpeed * horizontalInput);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject playerV;
+    public Camera frontCamera;
+    public Camera backCamera;
+    public Vector3 offset = new Vector3(0, 4, 3);
+    public Vector3 bOffset = new Vector3(0, 7, -4);
+    public bool reverse = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frontCamera.enabled = true;
+        backCamera.enabled = false;
+    }
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = playerV.transform.position + offset;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(reverse == false)
+            {
+                frontCamera.enabled = false;
+                backCamera.enabled = true;
+                reverse = true;
+                offset = bOffset;
+                transform.position = playerV.transform.position + offset;
+                Debug.Log("slash pressed");
+            }
+            else
+            {
+                backCamera.enabled = false;
+                frontCamera.enabled = true;
+                reverse = false;
+                offset = new Vector3(0, 4, 3);
+                transform.position = playerV.transform.position + offset;
+                Debug.Log("slash pressed");
+            }
+        }
+    }
+}
